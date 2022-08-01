255463ecb3b9f2a3c1445f006989dfd99cd58611
chakriya_met <metchakriya@gmail.com>
2022-08-01 17:15:15 +0700

    first version

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+user = int(input("Enter the number of sequence you want!: "))
+a = 2
+b = 9
+i =0
+result = 0
+for i in range (1, user+1):
+    if i%2 ==0:
+        n = -1
+    else: n =1
+    result = result + n* a / b
+    a +=3
+    b +=4
+print(result)
